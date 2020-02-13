fd42762eebf0c739dab8867f3af9229993d7b89b
Nihal Singh Verma <37439986+nihalsinghverma@users.noreply.github.com>
2020-02-13 13:09:46 +0530

    Update ironman.py

--- ironman.py
+++ ironman.py
@@ -1,12 +1,14 @@
-import mysql.connector
-import pandas as pd
-import datetime as dt
-from pyhive import presto
-from colorama import init,Fore, Back, Style 
+from avengers.pepperpotts import *
 
-
-#Initialization for colorama
-init() 
+"""
+=====Package Abbreviation====
+FC	:	Freight Cumulative
+FT	:	Freight Transacted
+SV	:	Services Verified or Registered
+ST	:	Services Transacted
+SVT	:	Services Voucher Transaction
+SWT	:	Services Wallet Transaction
+"""
 
 #Variable declaration
 tdate 	= dt.date.today().strftime('%Y%m%d')
@@ -16,85 +18,47 @@ df		= pd.DataFrame()
 print(Fore.WHITE+"We have lots of functions in Ironman package:\n 1. index_month & index_qtr for Month & Quarter Index\n 2. str_month & str_qtr for Month & Quarter Name\n 3. month_order_details & qtr_order_details for order level data\n 4. cohorts_dump for cohorst data\n"+Fore.RESET)
 
 def info():
+	"""This will give you a breif about this package."""
 	print(Fore.WHITE+"We have lots of functions in this package:\n 1. index_month & index_qtr for Month & Quarter Index\n 2. str_month & str_qtr for Month & Quarter Name\n 3. month_order_details & qtr_order_details for order level data\n 4. cohorts_dump for cohorst data\n"+Fore.RESET)
 
-#Sqlconnector for DbConnection
-con_zinka  = mysql.connector.connect(user='public', password='zinka@321', host='192.168.1.91', database='zlog')
-con_presto = presto.connect(host='192.168.1.1', port=8080)
-
-#Queries for data maipulation
-qry_trucksonplatformS	= "SELECT DISTINCT truck_no FROM divum.blackbuck.fleetApp_truck WHERE is_verified = 1"
-qry_trucksonplatformF	= "SELECT DISTINCT registration_number FROM od_snapshot WHERE registration_number <> ''"
-
-qry_truckstransactingS	= """
-SELECT DATE_FORMAT(updated_at+ INTERVAL '330' MINUTE, '%Y%m') AS month_name,
-trucks AS registration_number
-FROM
-(
-SELECT updated_at,
-truck_no
-FROM services_payment.blackbuck.wallet_wallettransactionhistory
-WHERE status IN ('FasTag Recharge',
-'FastageRecharge Failure',
-'HPCL-Card-Recharge',
-'HPCLCardless',
-'Reliance-Card-Recharge',
-'RelianceCardless',
-'Card-Recharge',
-'Card-Pull',
-'Card-less Pull')
-AND truck_no IS NOT NULL
-UNION ALL SELECT a.created_at AS updated_at,
-b.truck_no AS truck_no
-FROM divum.blackbuck.fleetApp_gpsrequest a
-INNER JOIN divum.blackbuck.fleetApp_truck b ON a.truck_id=b.id
-WHERE status_id = 4
-UNION ALL SELECT a.created_on AS updated_at,
-c.truck_no AS truck_no
-FROM divum.blackbuck.voucher_transaction a
-LEFT JOIN divum.blackbuck.voucher b ON a.voucher_id = b.id
-LEFT JOIN divum.blackbuck.fleetApp_truck c ON b.truck_id = c.id
-WHERE a.type = 'DEBIT'
-AND b.mode IN ('CASH', 'ACCOUNT') 
-) a
-GROUP BY 1,2
-"""
-
-qry_truckstransactingS	= qry_truckstransactingS.replace("\n", " ")
-qry_truckstransactingF	= "SELECT month_name, registration_number FROM od_snapshot WHERE registration_number <> ''"
-
 def list_month():
-    month = ['01','02','03','04','05','06','07','08','09','10','11','12']
-    for y in range(2015,2021):
-        for m in month:
-            if y == 2015 and int(m) <11:
-                pass
-            else:
-                yield(str(y)+m)
+	"""This will give you the list of months."""
+	month = ['01','02','03','04','05','06','07','08','09','10','11','12']
+	for y in range(2015,2021):
+		for m in month:
+			if y == 2015 and int(m) <11:
+				pass
+			else:
+				yield(str(y)+m)
 
 def str_month(month_index):
+	"""This will return month by passing the month index."""
 	return list(list_month())[month_index]
 
 def index_month(month_string):
+	"""This will give you the index of month for ex. 50 for 202001"""
 	return list(list_month()).index(month_string)
 
 def list_qtr():
-    yield('')
-    qtr = ['Q1','Q2','Q3','Q4']
-    for y in range(2015,2025):
-        for q in qtr:
-            if y == 2015 and q !='Q4':
-                pass
-            else:
-                yield(str(y)+"-"+q)
+	"""This will give you the list of months."""
+	qtr = ['Q1','Q2','Q3','Q4']
+	for y in range(2015,2025):
+		for q in qtr:
+			if y == 2015 and q !='Q4':
+				pass
+			else:
+				yield(str(y)+"-"+q)
 
 def str_qtr(qtr_index):
+	"""This will return quarter by passing the quarter index."""
 	return list(list_qtr())[qtr_index]
 
 def index_qtr(qtr_string):
+	"""This will give you the index of quarter for ex. 1 for 2015-12"""
 	return list(list_qtr()).index(qtr_string)
 
 def month_order_details(from_month='201501',to_month='201501'):
+	"""This will return a dataFrame and give you order level dump (filter on month)."""
 	fm = index_month(from_month)
 	tm = index_month(to_month)
 
@@ -103,6 +67,7 @@ def month_order_details(from_month='201501',to_month='201501'):
 	return dataframe
 
 def qtr_order_details(from_qtr='2015-Q1',to_qtr='2015-Q1'):
+	"""This will return a dataFrame and give you order level dump (filter on quarter)."""
 	fq = index_qtr(from_qtr)
 	tq = index_qtr(to_qtr)
 
@@ -110,7 +75,53 @@ def qtr_order_details(from_qtr='2015-Q1',to_qtr='2015-Q1'):
 	dataframe = pd.read_sql(statement,con=con_zinka)
 	return dataframe
 
+def getPResults(query):
+	"""Pass the quey in this function and it will return results in dataFrame from Presto DB"""
+	df = pd.read_sql_query(query, con_presto)
+	return df
+
+def getZResults(query):
+	"""Pass the quey in this function and it will return results in dataFrame from local DB"""
+	df = pd.read_sql_query(query, con_zinka)
+	return df
+
+def upload2Sql(dataFrame, dbName, tableName, mode='fail'):
+	"""Using this function you can upload the data to mysql server """
+
+	sqlEngine	   = create_engine('mysql+pymysql://py.user:py@1234@172.18.8.91/'+dbName, pool_recycle=3600)
+	dbConnection	= sqlEngine.connect()
+
+	try:
+		frame		   = dataFrame.to_sql(tableName, dbConnection, if_exists=mode);
+	except ValueError as vx:
+		print(vx)
+	except Exception as ex:   
+		print(ex)
+	else:
+		print("Table %s created successfully."%tableName);   
+	finally:
+		dbConnection.close()
+
+def common_sp_FC_SV():
+	"""This function will return string with common fleet owner or supply partner count b/w services verified and freight transacted"""
+	
+	df1 = getZResults(qry_sponplatformF)
+	df2 = getPResults(qry_sponplatformS)
+	df2['sp_number']= df2.sp_number.astype(str)
+	df2['month_name']=202001
+	df3 = df1.merge(df2, how='left', on='sp_number')
+	df1.to_csv('SPonPlatform-Freight.csv', index=False)
+	df2.to_csv('SPonPlatform-Service.csv', index=False)
+	df3.to_csv('CommonSP on Platform.csv', index=False)
+	
+	return "Common SPs in Services (verified) & Freight (Transacted cumulative):  "+str(df1.count()[0] - df3.isna().sum()[1])
+
+def common_sp_FC_ST():
+	pass
+
 def cohorts_dump(cohort_type,qtr_or_month):
+	"""This function will return a dataFrame, you'll have to pass cohort_type (shipper,carrier,cluster, sme or unit) & qtr_or_month (m or q)."""
+
 	if qtr_or_month.lower() == 'm':
 
 		if cohort_type.lower() == 'shipper' or cohort_type.lower() == 'customer':
@@ -133,7 +144,7 @@ def cohorts_dump(cohort_type,qtr_or_month):
 			print("Your monthly cohorts dump for "+cohort_type.lower()+"s is ready !!!"+Fore.RESET)
 
 		else:
-			print(Fore.YELLOW+"Please enter correct cohorts type (ie. shipper, carrier, cluster or sme) !!!"+Fore.RESET)
+			print(Fore.YELLOW+"Please enter correct cohorts type (ie. shipper, carrier, cluster, unit or sme) !!!"+Fore.RESET)
 
 	elif qtr_or_month.lower() == 'q':
 
@@ -150,9 +161,9 @@ def cohorts_dump(cohort_type,qtr_or_month):
 			print("Your quaterly cohorts dump for "+cohort_type.lower()+"s is ready !!!"+Fore.RESET)
 
 		else:
-			print(Fore.YELLOW+"Please enter correct cohorts type (ie. shipper, carrier, cluster or sme) !!!"+Fore.RESET)
+			print(Fore.YELLOW+"Please enter correct cohorts type (ie. shipper, carrier, cluster, unit or sme) !!!"+Fore.RESET)
 	
 	else:
-		print(Fore.YELLOW+"Please enter correct cohorts type (ie. shipper, carrier, cluster or sme) or/and correct type q or m for quartely and monthly cohorts respectively !!!"+Fore.RESET)
+		print(Fore.YELLOW+"Please enter correct cohorts type (ie. shipper, carrier, cluster, unit or sme) or/and correct type q or m for quartely and monthly cohorts respectively !!!"+Fore.RESET)
 
 
